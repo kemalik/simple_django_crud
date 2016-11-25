9a801098aa1a94d675ed8fd6acd2a85d531f091b
malik <kekenov.malik@gmail.com>
2016-11-25 18:44:08 +0600

    add image field, change charac field to description

--- applications/crud/admin.py
+++ applications/crud/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from applications.crud.models import Cars
 
-admin.site.register(Cars)
+class Cars_Admin(admin.ModelAdmin):
+    list_display = ['make', 'model', 'year']
+
+admin.site.register(Cars, Cars_Admin)
--- applications/crud/models.py
+++ applications/crud/models.py
@@ -27,7 +27,8 @@ class Cars(models.Model):
     make = models.CharField(u'Марка', max_length=50)
     model = models.CharField(u'Модель', max_length=50)
     year = models.CharField(u'Год выпуска', max_length=20, choices=YEARS_CHOICE)
-    characteristic = models.TextField(u'Характеристика')
+    description = models.TextField(u'Описание')
+    image = models.ImageField(u'Картинка', upload_to='images/cars')
 
     def __unicode__(self):
         return self.make
