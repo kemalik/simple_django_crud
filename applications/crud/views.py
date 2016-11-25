39411e1ee731f3ac4c93ed3a37d3dd07bc69f306
malik <kekenov.malik@gmail.com>
2016-11-25 20:12:18 +0600

    add delete car function

--- applications/crud/views.py
+++ applications/crud/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponseRedirect, HttpResponseNotFound
 from django.shortcuts import render
 from applications.crud.models import Cars
 from applications.crud.forms import AddCarForm
@@ -36,3 +36,23 @@ def add_car(request):
     }
 
     return render(request, 'add_car.html', context)
+
+
+def delete_car(request, pk):
+    confirm = request.GET.get('confirm')
+    try:
+        car = Cars.objects.get(pk=pk)
+    except:
+        return HttpResponseNotFound()
+
+    if confirm:
+        if confirm == 'yes':
+            car.delete()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/')
+    else:
+        context = {
+            'car': car
+        }
+        return render(request, 'delete_car.html', context)
