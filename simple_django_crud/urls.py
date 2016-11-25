3d4489bb3858d5bb1856a20071efa64232e5b015
malik <kekenov.malik@gmail.com>
2016-11-25 18:59:06 +0600

    add index page

--- simple_django_crud/urls.py
+++ simple_django_crud/urls.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
+from applications.crud.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name='index'),
 ]
 
 
