f956e62dd897346f7db20ff532231611e7bf43b6
malik <kekenov.malik@gmail.com>
2016-11-25 18:52:49 +0600

    add media settings

--- simple_django_crud/urls.py
+++ simple_django_crud/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
